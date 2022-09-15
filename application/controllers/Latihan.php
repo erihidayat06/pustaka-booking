05e51300225f23eb07db6e337b1840cb6d925815
eri hidayat <erihidayat549@gmail.com>
2022-09-15 16:58:42 +0700

    Upload awal project

---
+++ application/controllers/Latihan.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Latihan extends CI_Controller{
+
+    public function __construct()
+    {
+        parent::__construct();
+        //Codeigniter : Write Less Do More
+    }
+
+    function index()
+    {
+    echo "Selamat belajar web programing 2 Dengan kelas 12.3A.35";
+    }
+
+}
