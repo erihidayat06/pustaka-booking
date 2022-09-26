9dfa64e42bdf3954078de2d9a488c41a32c26f84
erihidayat06 <erihidayat549@gmial.com>
2022-09-26 09:19:29 +0700

    Mengubah bio data

--- application/views/v_about.php
+++ application/views/v_about.php
@@ -5,22 +5,22 @@ href="http://localhost/projek-eri/pustaka-booking/assets/css/stylebuku.css">
 
  <h4>Nama</h4>
  <ul type="disc">
- <li>Nama Depan : Imam</li>
- <li>Nama Belakang : Nawawi</li>
+ <li>Nama Depan : Eri</li>
+ <li>Nama Belakang : Hidayat</li>
  </ul>
  <br>
  <h4>Alamat</h4>
  <ul type="none">
- <li> Jalan Ciledug Raya No. 168 Pesanggrahan</li>
+ <li> Jalan Pecakran No. 53 Pasangan</li>
  </ul>
  
  <h4>Tempat Lahir</h4>
  <ul type="none">
- <li>Cirebon</li>
+ <li>Tegal</li>
  </ul>
  <h4>Olah Raga Favorit</h4>
  <ul type="square">
  <li>Bulutangkis</li>
- <li>Catur</li>
+ <li>Voli</li>
  </ul>
 </section>
